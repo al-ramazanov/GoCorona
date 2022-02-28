f5bcf24cb614dae92292a8c534334225cd31215e
Betlinsky <jipper114@gmail.com>
2022-02-28 21:19:33 +0300

    js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,30 @@ function openMenu() {
     })
 }
 openMenu();
+document.addEventListener('click', documentActions);
+function documentActions(e) {
+    const targetElement = e.target;
+    if (targetElement.closest('.blue__btn')) {
+        const modal = document.querySelector('.modal');
+        modal.classList.add('show');
+        const modalClose = document.querySelector('.modal__close');
+        modalClose.addEventListener('click', function () {
+            if (modal.classList.contains('show')) {
+                modal.classList.remove('show')
+            }
+
+        });
+        document.addEventListener('click', function (event) {
+            if (modal == event.target) {
+                modal.classList.remove('show')
+            }
+        })
+        document.addEventListener('keydown', function (e) {
+            if (e.key === 'Escape' && modal.classList.contains('show')) {
+                modal.classList.remove('show')
+            }
+        })
+
+    }
+
+}
